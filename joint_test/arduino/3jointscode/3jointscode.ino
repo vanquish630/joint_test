fcfd158ff8d277538d1bd700ef48dc72a81288fa
sohan <soboyisgr8@gmail.com>
2019-07-21 16:52:42 +0530

    arduino code  change

--- joint_test/arduino/3jointscode/3jointscode.ino
+++ joint_test/arduino/3jointscode/3jointscode.ino
@@ -9,6 +9,10 @@
 #include <sensor_msgs/ChannelFloat32.h>
 #include <sensor_msgs/JointState.h>
 
+float degree_to_rad(int deg){
+  return (3.14/180)*deg;
+  
+  }
 
 ros::NodeHandle  nh;
 
@@ -21,6 +25,9 @@ ros::Publisher joint_pub("joint_states", &joint_state);
 
 Servo wrist,elbow,shoulder;
 void setup(){
+ 
+    Serial.begin(115200);
+
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   shoulder.attach(9);
@@ -28,9 +35,9 @@ void setup(){
   wrist.attach(11);
   
   
-  shoulder.write(0);
-  elbow.write(0);
-  wrist.write(0);
+  shoulder.write(90);
+  elbow.write(90);
+  wrist.write(90);
  // char robot_id = "/arm_mesh";
   char *joint_names[] ={"link1_link2","link2_link3","link3_link4"}; 
   float pos[3]; /// stores arduino time
@@ -55,10 +62,31 @@ void setup(){
 
 void loop(){
    joint_state.header.stamp = nh.now();
-    joint_state.position[0] = 0;
-    joint_state.position[1] = 0;
-    joint_state.position[2] = 0;
-    //joint_state.header.stamp = ros::Time::now();
+   int readshoulder=analogRead(A1);
+   int readelbow=analogRead(A2);
+   int readwrist=analogRead(A3);
+
+   int sd = map(readshoulder,117,432,0,180);
+   Serial.println(sd);
+   int ed = map(readelbow,114,430,0,180);
+   Serial.println(ed);
+   int wd = map(readwrist,116,444,0,180);
+   Serial.println(wd);
+
+   float shoulder_angle = degree_to_rad(sd);
+   float elbow_angle=degree_to_rad(ed);
+   float wrist_angle=degree_to_rad(wd);
+   
+   
+    joint_state.position[0] = shoulder_angle-1.57;
+    joint_state.position[1] = elbow_angle-1.57;
+    joint_state.position[2] = wrist_angle-1.57;
+    
+  
+  
+  
+  
+  
     joint_pub.publish(&joint_state);
     nh.spinOnce();
 }
